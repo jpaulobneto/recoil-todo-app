7fc3b487c8831878582c7ecc32bf9ec7534f2983
João Paulo Barbosa <joao.barbosa@resultadosdigitais.com.br>
2020-07-21 14:35:14 -0300

    refactor: split components

---
+++ src/components/TodoList/TodoList.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { useRecoilValue } from "recoil";
+import { filteredTodoListState } from "../../state";
+import { TodoItem } from "../TodoItem";
+import { TodoItemCreator } from "../TodoItemCreator";
+import { TodoListFilters } from "../TodoListFilters";
+import { TodoListStats } from "../TodoListStats";
+
+export function TodoList() {
+  const todoList = useRecoilValue(filteredTodoListState);
+
+  return (
+    <>
+      <TodoListStats />
+      <TodoListFilters />
+      <TodoItemCreator />
+
+      {todoList.map((todoItem) => (
+        <TodoItem key={todoItem.id} item={todoItem} />
+      ))}
+    </>
+  );
+}
